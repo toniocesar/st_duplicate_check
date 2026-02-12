61af87e53c4c515ac95f20d983dd6deafc5a3eba
toniocesar <ant19ces97@gmail.com>
2026-02-12 12:48:50 +0100

    Update duplicate_check_alpha.py

--- duplicate_check_alpha.py
+++ duplicate_check_alpha.py
@@ -754,6 +754,7 @@ if st.button("Plot"):
    #             st.session_state.manager_vars
     #        )
 
+            st.write("chegamos aqui")
             #st.dataframe(styled_table, use_container_width=True)
             #st.table(styled_table)
             df = build_comparison_table_test_0(results)
