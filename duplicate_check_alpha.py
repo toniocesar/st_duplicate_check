e938893357ac1c024c35e57ff91014200a70b5d4
toniocesar <ant19ces97@gmail.com>
2026-02-12 13:16:30 +0100

    Update duplicate_check_alpha.py

--- duplicate_check_alpha.py
+++ duplicate_check_alpha.py
@@ -764,7 +764,7 @@ if st.button("Plot"):
             #st.dataframe(styled_table, use_container_width=True)
             #st.table(styled_table)
             df = build_comparison_table_test_0(results)
-            st.table(df)
+            st.dataframe(df)
 
 
 
